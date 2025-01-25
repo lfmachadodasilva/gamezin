5e938bb25aa649b7fd33a6eb026c396da308e000
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2025-01-25 19:55:51 +0000

    create project

---
+++ apps/games-web/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'games-web',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/games-web',
+};
